b9469209c081459c34bbc469ff09fdcd973b3203
Katerina Shevchenko <katyusha.shev18@gmail.com>
2022-11-17 20:33:46 +0200

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.scss";
 import PageHome from "../src/pages/PageHome/index";
+import Rout from "./routes/Rout";
 
 function App() {
   return (
-    <div>
-      <PageHome />
-    </div>
+    <>
+      <Rout />
+    </>
   );
 }
 
