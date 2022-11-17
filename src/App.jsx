643d1570bf9b56b7b42092c0dd9c94bb939d96a0
Katerina Shevchenko <katyusha.shev18@gmail.com>
2022-11-17 21:03:17 +0200

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.scss";
 import Rout from "./routes/Rout";
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <>
       <Header/>
-      <Rout />
+      <Rout/>
+      <Footer/>
     </>
   );
 }
