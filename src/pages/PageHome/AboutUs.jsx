86167873f12f004fa3c7fc303cc1ba7dec3dceea
Nazarii <nazariikubik@gmail.com>
2022-11-18 11:22:29 -0500

    configure Redux toolkit

--- src/pages/PageHome/AboutUs.jsx
+++ src/pages/PageHome/AboutUs.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import Title from "../../components/Title";
 
-
 const AboutUs = () => {
   return (
     <section className="container py-5">
       <Title title="About Us" subtitle="Who we are" />
-      <div class="row d-flex flex-column flex-md-row">
-        <div class="col d-flex flex-column gap-3 justify-content-between">
+      <div className="row d-flex flex-column flex-md-row">
+        <div className="col d-flex flex-column gap-3 justify-content-between">
           <p className="">
             We are young Ukrainian brand from Kyiv. We designs exclusive
             handmade lingerie, swimsuits and homeweare for women.
@@ -19,20 +18,21 @@ const AboutUs = () => {
             This campaign carries a valuable message of the importance of
             escaping unwanted captivity to achieve the highest good of all.
           </p>
-          <div class="d-flex gap-4">
+          <div className="d-flex gap-4">
             <img
-              class="img-fluid overflow-auto"
+              className="img-fluid overflow-auto"
               src="./images/home/about1.png"
               alt="about1"
             />
             <img
-              class="img-fluid overflow-auto"
+              className="img-fluid overflow-auto"
               src="./images/home/about2.png"
               alt="about2"
             />
           </div>
         </div>
-        <div class="col d-flex flex-column  justify-content-between">
+
+        <div className="col d-flex flex-column  justify-content-between">
           <p className="my-5 m-md-0">
             This campaign carries a valuable message of the importance of
             escaping unwanted captivity to achieve the highest good of all.
@@ -40,7 +40,11 @@ const AboutUs = () => {
             major point in the brand's intentions.
           </p>
 
-          <img class="img-fluid" src="./images/home/about3.png" alt="about3" />
+          <img
+            className="img-fluid"
+            src="./images/home/about3.png"
+            alt="about3"
+          />
         </div>
       </div>
     </section>
---
+++ src/store/store.js
@@ -0,0 +1,12 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import productSlice from "./products/productSlice";
+import userSlice from "./user/userSlice";
+
+const rootReducer = combineReducers({
+  user: userSlice,
+  products: productSlice,
+});
+
+export const store = configureStore({
+  reducer: rootReducer,
+});
