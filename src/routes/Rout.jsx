9748b1095022c9ed4ca557c3ca62c93a19eeb030
Katerina Shevchenko <katyusha.shev18@gmail.com>
2022-11-25 19:47:42 +0200

    edit router

--- src/routes/Rout.jsx
+++ src/routes/Rout.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Routes } from 'react-router';
+import PageFav from '../pages/PageFav';
 import PageHome from '../pages/PageHome';
 
 const Rout = () => {    
     return (
         <Routes>
             <Route exact='true' path='/' element={<PageHome/>}/>
+            <Route exact='true' path='/fav' element={<PageFav/>}/>
       </Routes>
     )
 }
