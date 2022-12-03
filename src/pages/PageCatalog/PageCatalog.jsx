1ca90cf6bd81c6141494f80e9d1e4578de02dad4
Katerina Shevchenko <katyusha.shev18@gmail.com>
2022-12-03 23:47:12 +0200

    add functionality in CatalogPage

--- src/pages/PageCatalog/PageCatalog.jsx
+++ src/pages/PageCatalog/PageCatalog.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../../components/Button';
 import ProductCard from '../../components/ProductCard';
 import { useSelector } from 'react-redux';
 import './PageCatalog.scss';
 import Title from '../../components/Title/Title';
+import Checkbox from '../../components/Checkbox';
 
 const PageCatalog = () => {
   const products = useSelector((state) => state.products);
+  const [sorlList, setSortList] = useState(false);
+
+  const categoryArray = ["Lingerie", "Swimwear", "Homewear"];
+  const colorsArray = ["Black", "White", "Creme", "Beige", "Red", "Gray", "DarkRed"];
+  const sizesArray = ["XS", "S", "M", "L", "XL", "XXL"];
+
+  const cklikOnSort = () => {
+    setSortList(!sorlList);
+  }
+
   return (
     <div className="container page">
       <nav className="page-nav">
@@ -15,241 +26,59 @@ const PageCatalog = () => {
           <li className="page-nav_item">Catalogue</li>
         </ul>
       </nav>
-      <Title subtitle="Catalogue" />
+      <Title subtitle="Catalogue"/>
       <div className="page-wrapper">
         <aside className="page-sidebar">
           <Title title="Category" />
           <ul className="page-filter_list">
-            <li className="page-filter_item">
-              <div className="form-check">
-                <input
-                  className="form-check-input  "
-                  type="checkbox"
-                  value=""
-                  id="flexCheckDefault"
-                />
-                <label className="form-check-label" for="flexCheckDefault">
-                  Lingerie
-                </label>
-              </div>
-            </li>
-            <li className="page-filter_item">
-              <div className="form-check">
-                <input
-                  className="form-check-input  "
-                  type="checkbox"
-                  value=""
-                  id="flexCheckDefault"
-                />
-                <label className="form-check-label" for="flexCheckDefault">
-                  Swimwear
-                </label>
-              </div>
-            </li>
-            <li className="page-filter_item">
-              <div className="form-check">
-                <input
-                  className="form-check-input  "
-                  type="checkbox"
-                  value=""
-                  id="flexCheckDefault"
-                />
-                <label className="form-check-label" for="flexCheckDefault">
-                  Homewear
-                </label>
-              </div>
-            </li>
+            {categoryArray && categoryArray.map((item, index) => {
+              return (
+                <li key={index}>
+                  <Checkbox label={item} id="flexCheckDefault"/>
+                </li>
+                )
+              })
+            }
           </ul>
-          <Title title="Colors" />
+          <Title title="Colors"/>
           <ul className="page-colors_list">
-            <li className="page-colors_item">
-              <div className="form-check">
-                <input
-                  className="form-check-input  "
-                  type="checkbox"
-                  value=""
-                  id="flexCheckDefault"
-                />
-                <label className="form-check-label" for="flexCheckDefault">
-                  <div className="color-square black"></div> Black
-                </label>
-              </div>
-            </li>
-            <li className="page-colors_item">
-              <div className="form-check">
-                <input
-                  className="form-check-input  "
-                  type="checkbox"
-                  value=""
-                  id="flexCheckDefault"
-                />
-                <label className="form-check-label" for="flexCheckDefault">
-                  <div className="color-square white"></div> White
-                </label>
-              </div>
-            </li>
-            <li className="page-colors_item">
-              <div className="form-check">
-                <input
-                  className="form-check-input  "
-                  type="checkbox"
-                  value=""
-                  id="flexCheckDefault"
-                />
-                <label className="form-check-label" for="flexCheckDefault">
-                  <div className="color-square creme"></div> Creme
-                </label>
-              </div>
-            </li>
-            <li className="page-colors_item">
-              <div className="form-check">
-                <input
-                  className="form-check-input  "
-                  type="checkbox"
-                  value=""
-                  id="flexCheckDefault"
-                />
-                <label className="form-check-label" for="flexCheckDefault">
-                  <div className="color-square beige"></div> Beige
-                </label>
-              </div>
-            </li>
-            <li className="page-colors_item">
-              <div className="form-check">
-                <input
-                  className="form-check-input  "
-                  type="checkbox"
-                  value=""
-                  id="flexCheckDefault"
-                />
-                <label className="form-check-label" for="flexCheckDefault">
-                  <div className="color-square red"></div> Red
-                </label>
-              </div>
-            </li>
-            <li className="page-colors_item">
-              <div className="form-check">
-                <input
-                  className="form-check-input  "
-                  type="checkbox"
-                  value=""
-                  id="flexCheckDefault"
-                />
-                <label className="form-check-label" for="flexCheckDefault">
-                  <div className="color-square gray"></div> Gray
-                </label>
-              </div>
-            </li>
-            <li className="page-colors_item">
-              <div className="form-check">
-                <input
-                  className="form-check-input  "
-                  type="checkbox"
-                  value=""
-                  id="flexCheckDefault"
-                />
-                <label className="form-check-label" for="flexCheckDefault">
-                  <div className="color-square darkred"></div> DarkRed
-                </label>
-              </div>
-            </li>
+          {colorsArray && colorsArray.map((item, index) => {
+              return (
+                <li key={index}>
+                  <Checkbox label={item} id="flexCheckDefault" colorSquare classForSquare={item.toLowerCase()}/>
+                </li>
+                )
+              })
+            }
             <p className="more-colors pseudo">More colors</p>
           </ul>
-          <Title title="Sizes" />
+          <Title title="Sizes"/>
           <ul className="page-sizes_list">
-            <li className="page-sizes_item">
-              <div className="form-check">
-                <input
-                  className="form-check-input  "
-                  type="checkbox"
-                  value=""
-                  id="flexCheckDefault"
-                />
-                <label className="form-check-label" for="flexCheckDefault">
-                  XS
-                </label>
-              </div>
-            </li>
-            <li className="page-sizes_item">
-              <div className="form-check">
-                <input
-                  className="form-check-input  "
-                  type="checkbox"
-                  value=""
-                  id="flexCheckDefault"
-                />
-                <label className="form-check-label" for="flexCheckDefault">
-                  S
-                </label>
-              </div>
-            </li>
-            <li className="page-sizes_item">
-              <div className="form-check">
-                <input
-                  className="form-check-input  "
-                  type="checkbox"
-                  value=""
-                  id="flexCheckDefault"
-                />
-                <label className="form-check-label" for="flexCheckDefault">
-                  M
-                </label>
-              </div>
-            </li>
-            <li className="page-sizes_item">
-              <div className="form-check">
-                <input
-                  className="form-check-input  "
-                  type="checkbox"
-                  value=""
-                  id="flexCheckDefault"
-                />
-                <label className="form-check-label" for="flexCheckDefault">
-                  L
-                </label>
-              </div>
-            </li>
-            <li className="page-sizes_item">
-              <div className="form-check">
-                <input
-                  className="form-check-input  "
-                  type="checkbox"
-                  value=""
-                  id="flexCheckDefault"
-                />
-                <label className="form-check-label" for="flexCheckDefault">
-                  XL
-                </label>
-              </div>
-            </li>
-            <li className="page-sizes_item">
-              <div className="form-check">
-                <input
-                  className="form-check-input  "
-                  type="checkbox"
-                  value=""
-                  id="flexCheckDefault"
-                />
-                <label className="form-check-label" for="flexCheckDefault">
-                  XXL
-                </label>
-              </div>
-            </li>
+          {sizesArray && sizesArray.map((item, index) => {
+              return (
+                <li key={index}>
+                  <Checkbox label={item} id="flexCheckDefault"/>
+                </li>
+                )
+              })
+            }
             <p className="more-sizes pseudo">More sizes</p>
           </ul>
           <Button text="Filter" className="page__button content-button" />
         </aside>
-        <section className="content">
-          <p className="content-sort">Sort by </p>
+        <section className="content cards">
+          <p className="content-sort" onClick={()=>cklikOnSort()}>Sort by </p>
 
-          <ul className="content-sort-list">
-            <li className="content-sort-item">Featured</li>
-            <li className="content-sort-item">Price: Low to High</li>
-            <li className="content-sort-item">Price: High to Low</li>
-            <li className="content-sort-item">Costumer Review</li>
-            <li className="content-sort-item">Newest Arrivals</li>
-          </ul>
-
+          {sorlList && 
+            <ul className="content-sort-list">
+              <li className="content-sort-item">Featured</li>
+              <li className="content-sort-item">Price: Low to High</li>
+              <li className="content-sort-item">Price: High to Low</li>
+              <li className="content-sort-item">Costumer Review</li>
+              <li className="content-sort-item">Newest Arrivals</li>
+            </ul>
+          }
+          
           <ul className="content-list">
             {products.length !== 0 ? (
               <>
@@ -276,4 +105,5 @@ const PageCatalog = () => {
     </div>
   );
 };
+
 export default PageCatalog;
