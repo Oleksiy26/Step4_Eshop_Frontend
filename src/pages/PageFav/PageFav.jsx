a69cef1b66e5eedf0b3072ed6e4219b7d887f903
Katerina Shevchenko <katyusha.shev18@gmail.com>
2022-12-06 23:27:45 +0200

    fixed favpage

--- src/pages/PageFav/PageFav.jsx
+++ src/pages/PageFav/PageFav.jsx
@@ -4,35 +4,35 @@ import ContainerFav from "./ContainerFav/ContainerFav";
 import { useDispatch } from "react-redux";
 import { checkLocation } from "../../store/location/location";
 import { useLocation } from "react-router-dom";
+import Title from '../../components/Title/Title';
 
 const PageFav = () => {
     const products = useSelector((state) => state.products);
     const dispatch = useDispatch();
     const location = useLocation();
+    const favCounter = useSelector((state) => state.counter)
   
     useEffect(() => {
       dispatch(checkLocation(location.pathname))
     }, []);
 
     const findItemsFav = () => {
-        const itemsCart = JSON.parse(localStorage.getItem('fav'));
+        const itemsFav = JSON.parse(localStorage.getItem('fav'));
 
-        return products.products.reduce((arrayCart, item) => {
-            if (Array.isArray(itemsCart)) {
-                if (itemsCart.includes(item._id)) {
-                    arrayCart.push(item)
-                }
-            }
-            return arrayCart;
-        }, []);
+        if (itemsFav) {
+            return products.products.filter(item => itemsFav.includes(item._id))
+        }
     }
 
     return (
-        <>
+        <div className="container">
+        <Title subtitle={
+            favCounter.inFav ? "Your favorited cards" : "No cards in favourites"
+        }/>
         <ContainerFav
             items={findItemsFav()}
         />
-        </>
+        </div>
     )
 }
 
