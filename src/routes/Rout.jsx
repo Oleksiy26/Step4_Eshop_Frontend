1e8068904e8c3d6f017d0a8323e9b0ec6cfae917
Nazarii <nazariikubik@gmail.com>
2022-12-09 11:34:42 -0500

    Add Item page

--- src/routes/Rout.jsx
+++ src/routes/Rout.jsx
@@ -1,21 +1,23 @@
-import React from 'react';
-import { Route, Routes } from 'react-router';
-import PageFav from '../pages/PageFav';
-import PageHome from '../pages/PageHome';
-import PageCatalog from '../pages/PageCatalog';
-import Page404 from '../pages/Page404';
-import PageCart from '../pages/PageCart';
+import React from "react";
+import { Route, Routes } from "react-router";
+import PageFav from "../pages/PageFav";
+import PageHome from "../pages/PageHome";
+import PageCatalog from "../pages/PageCatalog";
+import Page404 from "../pages/Page404";
+import PageCart from "../pages/PageCart";
+import { PageItem } from "../pages/PageItem/PageItem";
 
-const Rout = () => {    
-    return (
-      <Routes>
-        <Route exact="true" path="/" element={<PageHome />} />
-        <Route exact="true" path="/catalog" element={<PageCatalog />} />
-        <Route exact="true" path="/fav" element={<PageFav />} />
-        <Route exact="true" path="/cart" element={<PageCart />} />
-        <Route path="*" element={<Page404 />} />
-      </Routes>
-    );
-}
+const Rout = () => {
+  return (
+    <Routes>
+      <Route exact="true" path="/" element={<PageHome />} />
+      <Route exact="true" path="/catalog" element={<PageCatalog />} />
+      <Route exact="true" path="/fav" element={<PageFav />} />
+      <Route exact="true" path="/cart" element={<PageCart />} />
+      <Route exact="true" path="/:id" element={<PageItem />} />
+      <Route path="*" element={<Page404 />} />
+    </Routes>
+  );
+};
 
 export default Rout;
