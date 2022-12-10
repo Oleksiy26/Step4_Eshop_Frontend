4c021a5977483fa58e7400ed94d34102499e8f8c
Oleksii Lubianyi <alexl30.al@gmail.com>
2022-12-10 20:39:41 +0200

    last changes

--- client/src/components/Category/Category.jsx
+++ client/src/components/Category/Category.jsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import Checkbox from '../Checkbox';
 import './Category.scss';
 
 const Category = () => {
-  const categoryArray = ['Lingerie', 'Swimwear', 'Homewear'];
+  const products = useSelector(state => state.products);
+  const [categoryArr, setcategoryArr] = useState([]);
+  console.log(products.products);
+
+  useEffect(() => {
+    const categoryArr = products.products.map(item => item.category);
+
+    console.log(categoryArr);
+    const existingCategory = new Set(categoryArr);
+    setcategoryArr(Array.from(existingCategory));
+  }, [products]);
+
   return (
     <ul className="page-filter_list">
-      {categoryArray &&
-        categoryArray.map((item, index) => {
+      {categoryArr &&
+        categoryArr.map((item, index) => {
           return (
-            <li className="page-filter_item" key={index}>
+            <li
+              className="page-filter_item"
+              key={index}
+              // onChange={() => onChangeCategory(categoryName)}
+            >
               <Checkbox label={item} id="flexCheckDefault" />
             </li>
           );
