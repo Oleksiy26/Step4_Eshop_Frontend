fbccbf58fe022fc82c1433a460f06cb413ed1446
Katerina Shevchenko <katyusha.shev18@gmail.com>
2023-01-09 21:33:05 +0200

    fixed check values in PageItem"

--- client/src/pages/PageItem/PageItem.jsx
+++ client/src/pages/PageItem/PageItem.jsx
@@ -1,28 +1,19 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { useNavigate, useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import Slider from 'react-slick'
-import ProductCard from '../../components/ProductCard'
 import { fetchCard } from '../../store/card/ActionCreator'
 import AddCartFavorit from '../../components/ProductCard/AddCartFavorit'
 import './PageItem.scss'
-import { getRandomRange } from '../../components/RandomRange/randomRange'
 import AlsoLike from '../../components/AlsoLike/AlsoLike'
+import Loader from '../../components/Loader'
+import Title from '../../components/Title'
 
 export const PageItem = () => {
   const { itemNo } = useParams()
-  const navigate = useNavigate()
   const dispatch = useDispatch()
-  const products = useSelector(state => state.products)
-  // const product = useSelector((state) => state.products.products[id]);
-  let arrayofProducts = products.products.length
-  let res = getRandomRange(0, arrayofProducts, 5)
-  // console.log(products)
   const { card, isCardLoading, cardError } = useSelector(state => state.card)
-
   const { imageUrls, name, currentPrice, size, color } = card
-  // console.log(card)
-  // console.log('sizes: ', sizes)
 
   useEffect(() => {
     dispatch(fetchCard(itemNo))
@@ -41,40 +32,44 @@ export const PageItem = () => {
 
   return (
     <div className='container'>
-      {isCardLoading && <h1 style={{ textAlign: 'center' }}>Loading...</h1>}
-      {cardError && <h1>Smth wrong happened</h1>}
-      <div className='my-5 d-flex gap-5 justify-content-center'>
-        <div className='sliderImg'>
-          <Slider {...settings}>
-            {card !== undefined
-              ? imageUrls?.map(el => (
-                  <img key={el} className='sliderImg' src={el} alt='' />
-                ))
-              : null}
-          </Slider>
-        </div>
-        <div>
-          <h1>{card !== undefined && name}</h1>
-          <h3>Available</h3>
-          <h4 className='my-4'>{card !== undefined && currentPrice}$</h4>
-          <div className='d-flex gap-4'>
-            <p>Available Sizes: </p>
+      {isCardLoading ? (
+        <Loader />
+      ) : cardError ? (
+        <Title subtitle='Smth wrong happened' />
+      ) : (
+        <div className='my-5 d-flex gap-5 justify-content-center'>
+          <div className='sliderImg'>
+            {imageUrls ? (
+              <Slider {...settings}>
+                {imageUrls?.map(el => (
+                  <img key={el} className='sliderImg' src={el} alt={name} />
+                ))}
+              </Slider>
+            ) : null}
+          </div>
+          <div>
+            <h1>{name && name}</h1>
+            <h3>Available</h3>
+            <h4 className='my-4'>{currentPrice && currentPrice}$</h4>
             <div className='d-flex gap-4'>
-              {size ? <p>{size}</p> : <>No sizes</>}
+              <p>Available Sizes: </p>
+              <div className='d-flex gap-4'>
+                {size ? <p>{size}</p> : <>No sizes</>}
+              </div>
             </div>
+            <div className='d-flex align-items-center'>
+              <p className='my-2'>Color:</p>
+              <span
+                style={{
+                  backgroundColor: color ? color : null
+                }}
+                className={'colorSquare'}
+              />
+            </div>
+            <AddCartFavorit />
           </div>
-          <div className='d-flex align-items-center'>
-            <p className='my-2'>Color:</p>
-            <span
-              style={{
-                backgroundColor: card !== undefined ? color : null
-              }}
-              className={'colorSquare'}
-            />
-          </div>
-          <AddCartFavorit />
         </div>
-      </div>
+      )}
       <div className='item-galery-wrapper'>
         <AlsoLike />
       </div>
