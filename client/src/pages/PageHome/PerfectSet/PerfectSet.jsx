038845ed4b16c7eabc108d097390ad887db563fc
Oleksii Lubianyi <alexl30.al@gmail.com>
2023-01-09 13:42:22 +0200

    random load

--- client/src/pages/PageHome/PerfectSet/PerfectSet.jsx
+++ client/src/pages/PageHome/PerfectSet/PerfectSet.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import DiscoverLink from '../../../components/DiscoverLink/DiscoverLink'
 import Loader from '../../../components/Loader'
@@ -9,9 +9,14 @@ import './index.scss'
 
 const PerfectSet = () => {
   const products = useSelector(state => state.products)
+  const [productsCount, setProductCount] = useState(0)
+  const [randomRange, setRandomRange] = useState(0)
 
-  let arrayofProducts = products.products.length
-  let res = getRandomRange(0, arrayofProducts, 4)
+  useEffect(() => {
+    setProductCount(products.products.length)
+    setRandomRange(getRandomRange(0, productsCount, 4))
+  }, [products.products, productsCount])
+
   return (
     <div className='container'>
       <Title title='new arrivals' subtitle='Choose your perfect set' />
@@ -20,19 +25,21 @@ const PerfectSet = () => {
           <Loader />
         ) : (
           <section className='set'>
-            {products.products.slice(res.start, res.end).map(item => (
-              <ProductCard
-                ident={item.itemNo}
-                price={item.currentPrice}
-                photoUrl={item.imageUrls[0]}
-                subClass={'set-item'}
-                key={item._id}
-                id={item._id}
-                nameCard={item.name}
-                color={item.color}
-                size={item.size}
-              />
-            ))}
+            {products.products
+              .slice(randomRange.start, randomRange.end)
+              .map(item => (
+                <ProductCard
+                  ident={item.itemNo}
+                  price={item.currentPrice}
+                  photoUrl={item.imageUrls[0]}
+                  subClass={'set-item'}
+                  key={item._id}
+                  id={item._id}
+                  nameCard={item.name}
+                  color={item.color}
+                  size={item.size}
+                />
+              ))}
             <DiscoverLink subClass={'set-link set-item'} />
           </section>
         )
