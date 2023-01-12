366fe9db96bb901d7a18d28d703a8b6cbfa3cfc7
Nazarii <nazariikubik@gmail.com>
2023-01-11 22:10:58 -0500

    Fix instagram style

--- client/src/pages/PageHome/Instagram/Instagram.jsx
+++ client/src/pages/PageHome/Instagram/Instagram.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Title from '../../../components/Title'
+import './Instagram.scss'
 
 const Instagram = () => {
   return (
@@ -10,12 +11,14 @@ const Instagram = () => {
           <h3 className='text-uppercase fs-3'>@femlingerie</h3>
         </div>
       </div>
-      <div className='d-flex gap-4 flex-column flex-md-row'>
-        <img
-          className='img-fluid overflow-auto flex-grow-1'
-          src='./images/home/inst_img.png'
-          alt=''
-        />
+      <div className='imgSection d-flex gap-4 align-items-center flex-column flex-md-row'>
+        <div className='subSections'>
+          <img
+            className='img-fluid overflow-auto flex-grow-1'
+            src='./images/home/inst_img.png'
+            alt=''
+          />
+        </div>
         <div className='d-flex gap-4'>
           <img
             className='img-fluid overflow-auto flex-grow-1'
@@ -29,11 +32,13 @@ const Instagram = () => {
             alt=''
           />
         </div>
-        <img
-          className='img-fluid overflow-auto flex-grow-1'
-          src='./images/home/inst_img-3.png'
-          alt=''
-        />
+        <div className='subSections'>
+          <img
+            className='img-fluid overflow-auto flex-grow-1'
+            src='./images/home/inst_img-3.png'
+            alt=''
+          />
+        </div>
       </div>
     </section>
   )
