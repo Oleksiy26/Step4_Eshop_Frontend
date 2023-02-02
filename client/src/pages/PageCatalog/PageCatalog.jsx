7768bbc6099910b7c1600ab5fe78df1529a3c4b7
Oleksii Lubianyi <alexl30.al@gmail.com>
2023-02-02 16:42:41 +0200

    push to main

--- client/src/pages/PageCatalog/PageCatalog.jsx
+++ client/src/pages/PageCatalog/PageCatalog.jsx
@@ -23,11 +23,12 @@ const PageCatalog = () => {
     state => state.filter
   )
   const pagesCount = Math.ceil(products.productsQuantity / perPage)
+  const currentWidth = useWindowSize()
 
   const [sizesActive, setsizesActive] = useState(true)
   const [colorActive, setcolorActive] = useState(true)
   const [categoryActive, setcategoryActive] = useState(true)
-  const [filtersIsOpen, setfiltersIsOpen] = useState(true)
+  // const [filtersIsOpen, setfiltersIsOpen] = useState(false)
 
   function useWindowSize() {
     const [displayWidth, setdisplayWidth] = useState(0)
@@ -45,24 +46,15 @@ const PageCatalog = () => {
   const showColor = () => setcolorActive(!colorActive)
   const showSizes = () => setsizesActive(!sizesActive)
   const showCategory = () => setcategoryActive(!categoryActive)
-  const showFilters = () => setfiltersIsOpen(!filtersIsOpen)
+  // const showFilters = () => setfiltersIsOpen(!filtersIsOpen)
 
   const dispatch = useDispatch()
-
-  const currentWidth = useWindowSize()
-
   useEffect(() => {
     currentWidth < 768 ? dispatch(setperPage(2)) : dispatch(setperPage(6))
   }, [currentWidth, dispatch])
 
-  // currentWidth < 768
-  //   ? setfiltersIsOpen(!filtersIsOpen)
-  //   : setfiltersIsOpen
-
-  console.log(filtersIsOpen)
-
   const handleChangeMin = event => {
-    dispatch(setMinPrice(event.target.value))``
+    dispatch(setMinPrice(event.target.value))
   }
   const handleChangeMax = event => {
     dispatch(setMaxPrice(event.target.value))
@@ -84,44 +76,40 @@ const PageCatalog = () => {
       <BreadCrumbs startFrom='Home' />
       <Title subtitle='Catalogue' />
       <div className='page-wrapper'>
-        <Title
+        {/* <Title
           title='Filters box'
           showContent={showFilters}
           className={'filtering'}
-        />
-        {filtersIsOpen && (
-          <aside className='page-sidebar'>
-            <Title
-              title='Category'
-              showContent={showCategory}
-              className={'active'}
-            />
-            <Category
-              categoryActive={
-                currentWidth < 768 ? !categoryActive : categoryActive
-              }
-            />
-            <Title
-              title='Colors'
-              showContent={showColor}
-              className={'active'}
-            />
-            <Colors
-              contentActive={currentWidth < 768 ? !colorActive : colorActive}
-            />
-            <Title title='Sizes' showContent={showSizes} className={'active'} />
-            <Sizes
-              sizesActive={currentWidth < 768 ? !sizesActive : sizesActive}
-            />
-            <MinMaxFilter
-              minPrice={minPrice}
-              maxPrice={maxPrice}
-              onChange={event => handleInput(event)}
-              onChangeInputMin={event => handleChangeMin(event)}
-              onChangeInputMax={event => handleChangeMax(event)}
-            />
-          </aside>
-        )}
+        /> */}
+        {/* {!filtersIsOpen && ( */}
+        <aside className='page-sidebar'>
+          <Title
+            title='Category'
+            showContent={showCategory}
+            className={'active'}
+          />
+          <Category
+            categoryActive={
+              currentWidth < 768 ? !categoryActive : categoryActive
+            }
+          />
+          <Title title='Colors' showContent={showColor} className={'active'} />
+          <Colors
+            contentActive={currentWidth < 768 ? !colorActive : colorActive}
+          />
+          <Title title='Sizes' showContent={showSizes} className={'active'} />
+          <Sizes
+            sizesActive={currentWidth < 768 ? !sizesActive : sizesActive}
+          />
+          <MinMaxFilter
+            minPrice={minPrice}
+            maxPrice={maxPrice}
+            onChange={event => handleInput(event)}
+            onChangeInputMin={event => handleChangeMin(event)}
+            onChangeInputMax={event => handleChangeMax(event)}
+          />
+        </aside>
+        {/* )} */}
         <section className='content cards'>
           <SortList />
           <Galery />
